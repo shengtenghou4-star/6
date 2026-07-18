a33d326e967c56e13eb8fc96edc6264959ee5d1d
shengtenghou4-star <shengtenghou4@gmail.com>
2026-07-18 22:53:00 +0800

    Test stateful Betfair definition carryover

--- tests/test_betfair_historical.py
+++ tests/test_betfair_historical.py
@@ -5,6 +5,7 @@ import json
 from pathlib import Path
 
 from marketlab.sources.betfair_historical import (
+    BetfairStreamNormalizer,
     flatten_market_change_message,
     iter_bz2_updates,
     publish_time_from_ms,
@@ -31,6 +32,11 @@ def test_definition_only_message_is_preserved() -> None:
                     "status": "OPEN",
                     "inPlay": False,
                     "eventName": "Jamaica v Venezuela",
+                    "runners": [
+                        {"id": 54786, "name": "Jamaica"},
+                        {"id": 15302, "name": "Venezuela"},
+                        {"id": 58805, "name": "The Draw"},
+                    ],
                 },
             }
         ],
@@ -73,18 +79,74 @@ def test_runner_price_ladders_are_normalized() -> None:
     assert update.traded_volume == [[2.18, 500.0]]
 
 
-def test_bz2_line_stream(tmp_path: Path) -> None:
+def test_stateful_normalizer_carries_market_and_runner_identity() -> None:
+    normalizer = BetfairStreamNormalizer()
+    definition = {
+        "op": "mcm",
+        "pt": 1719210499670,
+        "mc": [
+            {
+                "id": "1.229978426",
+                "marketDefinition": {
+                    "eventId": "33356803",
+                    "eventName": "Jamaica v Venezuela",
+                    "marketType": "MATCH_ODDS",
+                    "marketTime": "2024-07-01T00:00:00.000Z",
+                    "status": "OPEN",
+                    "inPlay": False,
+                    "runners": [
+                        {"id": 54786, "name": "Jamaica"},
+                        {"id": 15302, "name": "Venezuela"},
+                        {"id": 58805, "name": "The Draw"},
+                    ],
+                },
+            }
+        ],
+    }
+    normalizer.consume(definition)
+
+    later_price_only = {
+        "op": "mcm",
+        "pt": 1719210599670,
+        "mc": [{"id": "1.229978426", "rc": [{"id": 54786, "ltp": 2.18}]}],
+    }
+    update = normalizer.consume(later_price_only)[0]
+    assert update.event_id == "33356803"
+    assert update.event_name == "Jamaica v Venezuela"
+    assert update.market_type == "MATCH_ODDS"
+    assert update.runner_name == "Jamaica"
+    assert update.last_traded_price == 2.18
+
+
+def test_bz2_line_stream_preserves_definition_state(tmp_path: Path) -> None:
     path = tmp_path / "market.bz2"
     messages = [
         {"op": "status", "statusCode": "SUCCESS"},
-        {"op": "mcm", "pt": 1719210499670, "mc": [{"id": "1.1", "rc": [{"id": 1, "ltp": 3.5}]}]},
+        {
+            "op": "mcm",
+            "pt": 1719210499670,
+            "mc": [
+                {
+                    "id": "1.1",
+                    "marketDefinition": {
+                        "eventId": "e1",
+                        "eventName": "Home v Away",
+                        "marketType": "MATCH_ODDS",
+                        "runners": [{"id": 1, "name": "Home"}],
+                    },
+                }
+            ],
+        },
+        {"op": "mcm", "pt": 1719210599670, "mc": [{"id": "1.1", "rc": [{"id": 1, "ltp": 3.5}]}]},
     ]
     with bz2.open(path, "wt", encoding="utf-8") as fh:
         for message in messages:
             fh.write(json.dumps(message) + "\n")
 
     updates = list(iter_bz2_updates(path))
-    assert len(updates) == 1
-    assert updates[0].market_id == "1.1"
-    assert updates[0].runner_id == 1
-    assert updates[0].last_traded_price == 3.5
+    assert len(updates) == 2
+    assert updates[1].market_id == "1.1"
+    assert updates[1].event_id == "e1"
+    assert updates[1].runner_id == 1
+    assert updates[1].runner_name == "Home"
+    assert updates[1].last_traded_price == 3.5
